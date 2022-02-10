ece7b85a0491105bcb17739798190989c5b547c9
titrabui <titrabui@gmail.com>
2022-02-11 02:01:56 +0700

    improve project contract

--- contracts/CashTestToken.sol
+++ contracts/CashTestToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./libraries/Config.sol";
 
 contract CashTestToken is ERC20 {
-    constructor(address[] memory acc) public ERC20("USDC Test Token", "USDC") {
+    constructor(address[] memory acc) public ERC20("USD Test Token", "USD") {
         for (uint256 i = 0; i < acc.length; i++) {
             _mint(acc[i], 100000000 * Constant.FIXED_POINT);
         }
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -4,182 +4,283 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
 import "./libraries/Formula.sol";
 import "./libraries/Config.sol";
 
-interface ITToken {
-    function stakeMint(address receiver, uint256 amount) external;
-}
-
 contract Project is Ownable {
     struct UserInfo {
-        uint256 lastAction;
-        uint256 originValue;
-        uint256 value;
-        bool isWhiteList;
+        uint256 stakedAmount;
+        uint256 fundedAmount;
+        uint256 tokenAllocationAmount;
+        bool isClaimedBack;
     }
 
     struct ProjectInfo {
         uint256 id;
-        address token;
-        uint256 startStakingTime;
-        uint256 endStakingTime;
-        uint256 startFundingTime;
-        uint256 endFundingTime;
-        uint256 totalAmountStake;
-        uint256 minAllocation;
-        uint256 maxAllocation;
-    } 
+        address tokenAddress;
+        uint256 allocaltionSize;
+        uint256 estimateTokenAllocationRate;
 
-    ProjectInfo[] public projects;
+        // Staking
+        uint256 stakingStartBlockNumber;
+        uint256 stakingEndBlockNumber;
+        uint256 stakingLimitAmount;
+        uint256 stakedTotalAmount;
+        address[] stakedAccounts;
+
+        // Funding
+        uint256 fundingStartBlockNumber;
+        uint256 fundingEndBlockNumber;
+        uint256 fundingMinAllocation;
+        uint256 fundingAllocationRate;
+    }
 
     IERC20 public gmi;
     IERC20 public busd;
 
-    // estimate
-    uint256 public ref;
+    uint256 public latestProjectId;
+    uint256[] public projectIds;
 
-    mapping(address => mapping(uint256 => UserInfo)) public userInfo;
-    mapping(uint256 => ProjectInfo) public projectInfo;
-    mapping(address => mapping(uint256 => bool)) public whiteList;
+    // projectId => project info
+    mapping(uint256 => ProjectInfo) public projects;
 
-    constructor(
-        IERC20 _gmi,
-        IERC20 _busd
-    ) {
+    // projectId => account address => user info
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+
+    // projectId => account address => is in whitelist
+    mapping(uint256 => mapping(address => bool)) public whiteLists;
+
+    event CreateProject(ProjectInfo project);
+    event SetAllocaltionSize(uint256 indexed _projectId, uint256 allocaltionSize);
+    event SetEstimateTokenAllocationRate(uint256 indexed _projectId, uint256 estimateTokenAllocationRate);
+    event SetStakingStartBlockNumber(uint256 indexed projectId, uint256 blockNumber);
+    event SetStakingEndBlockNumber(uint256 indexed projectId, uint256 blockNumber);
+    event SetStakingLimitAmount(uint256 indexed projectId, uint256 stakingLimitAmount);
+    event SetFundingStartBlockNumber(uint256 indexed projectId, uint256 blockNumber);
+    event SetFundingEndBlockNumber(uint256 indexed projectId, uint256 blockNumber);
+    event SetFundingMinAllocation(uint256 indexed projectId, uint256 minAllocation);
+    event SetFundingAllocationRate(uint256 indexed projectId, uint256 fundingAllocationRate);
+    event Stake(address account, uint256 indexed projectId, uint256 indexed amount);
+    event ClaimBack(address account, uint256 indexed projectId, uint256 indexed amount);
+    event AddedToWhitelist(uint256 indexed projectId, address[] accounts);
+    event RemovedFromWhitelist(uint256 indexed projectId, address indexed account);
+    event Funding(address account, uint256 indexed projectId, uint256 indexed amount, uint256 tokenAllocationAmount);
+
+    constructor(IERC20 _gmi, IERC20 _busd) {
         gmi = _gmi;
         busd = _busd;
     }
 
-    modifier onlyWhitelisted(uint256 projectId) { 
-        require(isWhitelisted(projectId, msg.sender)); 
+    modifier validProject(uint256 _projectId) {
+        require(_projectId <= latestProjectId, "Invalid project id");
         _; 
     }
 
-    event Deposit(
-        uint256 indexed amount,
-        uint256 lastAction
-    );
+    function createProject(
+        address _tokenAddress,
+        uint256 _allocaltionSize,
+        uint256 _estimateTokenAllocationRate,
+        uint256 _stakingStartBlockNumber,
+        uint256 _stakingEndBlockNumber,
+        uint256 _stakingLimitAmount,
+        uint256 _fundingStartBlockNumber,
+        uint256 _fundingEndBlockNumber,
+        uint256 _fundingMinAllocation,
+        uint256 _fundingAllocationRate
+    ) external onlyOwner {
+        ProjectInfo memory project;
+        project.id = latestProjectId;
+        project.tokenAddress = _tokenAddress;
+        project.allocaltionSize = _allocaltionSize;
+        project.estimateTokenAllocationRate = _estimateTokenAllocationRate;
+        project.stakingStartBlockNumber = _stakingStartBlockNumber;
+        project.stakingEndBlockNumber = _stakingEndBlockNumber;
+        project.stakingLimitAmount = _stakingLimitAmount;
+        project.fundingStartBlockNumber = _fundingStartBlockNumber;
+        project.fundingEndBlockNumber = _fundingEndBlockNumber;
+        project.fundingMinAllocation = _fundingMinAllocation;
+        project.fundingAllocationRate = _fundingAllocationRate;
 
-    /// @notice deposit amount of GMI tokens to Staking Pool
+        projects[latestProjectId] = project;
+        projectIds.push(latestProjectId);
+        latestProjectId++;
+        emit CreateProject(project);
+    }
+
+    function setAllocaltionSize(uint256 _projectId, uint256 _allocaltionSize) external onlyOwner validProject(_projectId) {
+        require(_allocaltionSize > 0, "Invalid project allocaltion size");
+
+        projects[_projectId].allocaltionSize = _allocaltionSize;
+        emit SetAllocaltionSize(_projectId, _allocaltionSize);
+    }
+
+    function setEstimateTokenAllocationRate(uint256 _projectId, uint256 _estimateTokenAllocationRate) external onlyOwner validProject(_projectId) {
+        require(_estimateTokenAllocationRate > 0, "Invalid project estimate token allocaltion rate");
+
+        projects[_projectId].estimateTokenAllocationRate = _estimateTokenAllocationRate;
+        emit SetEstimateTokenAllocationRate(_projectId, _estimateTokenAllocationRate);
+    }
+
+    function setStakingStartBlockNumber(uint256 _projectId, uint256 _blockNumber) external onlyOwner validProject(_projectId) {
+        require(_blockNumber > block.number, "Invalid block number");
+
+        projects[_projectId].stakingStartBlockNumber = _blockNumber;
+        emit SetStakingStartBlockNumber(_projectId, _blockNumber);
+    }
+
+    function setStakingEndBlockNumber(uint256 _projectId, uint256 _blockNumber) external onlyOwner validProject(_projectId) {
+        require(_blockNumber > block.number, "Invalid block number");
+
+        projects[_projectId].stakingEndBlockNumber = _blockNumber;
+        emit SetStakingEndBlockNumber(_projectId, _blockNumber);
+    }
+
+    function setStakingLimitAmount(uint256 _projectId, uint256 _stakingLimitAmount) external onlyOwner validProject(_projectId) {
+        require(_stakingLimitAmount > 0, "Invalid limit of stake amount");
+
+        projects[_projectId].stakingLimitAmount = _stakingLimitAmount;
+        emit SetStakingLimitAmount(_projectId, _stakingLimitAmount);
+    }
+
+    function setFundingStartBlockNumber(uint256 _projectId, uint256 _blockNumber) external onlyOwner validProject(_projectId) {
+        require(_blockNumber > block.number, "Invalid block number");
+
+        projects[_projectId].fundingStartBlockNumber = _blockNumber;
+        emit SetFundingStartBlockNumber(_projectId, _blockNumber);
+    }
+
+    function setFundingEndBlockNumber(uint256 _projectId, uint256 _blockNumber) external onlyOwner validProject(_projectId) {
+        require(_blockNumber > block.number, "Invalid block number");
+
+        projects[_projectId].fundingEndBlockNumber = _blockNumber;
+        emit SetFundingEndBlockNumber(_projectId, _blockNumber);
+    }
+
+    function setFundingMinAllocation(uint256 _projectId, uint256 _minAllocation) external onlyOwner validProject(_projectId) {
+        require(_minAllocation > 0, "Invalid project funding min allocaltion");
+
+        projects[_projectId].fundingMinAllocation = _minAllocation;
+        emit SetFundingMinAllocation(_projectId, _minAllocation);
+    }
+
+    function setFundingAllocationRate(uint256 _projectId, uint256 _fundingAllocationRate) external onlyOwner validProject(_projectId) {
+        require(_fundingAllocationRate > 0, "Invalid project funding allocaltion rate");
+
+        projects[_projectId].fundingAllocationRate = _fundingAllocationRate;
+        emit SetFundingAllocationRate(_projectId, _fundingAllocationRate);
+    }
+
+    /// @notice stake amount of GMI tokens to Staking Pool
     /// @dev    this method can called by anyone
     /// @param  _projectId  id of the project
     /// @param  _amount  amount of the tokens to be staked
-    function deposit(uint256 _projectId, uint256 _amount) external {
-        require(_projectId > 0, "Invalid project id");
-        require(_amount > 0, "Not enough amount");
+    function stake(uint256 _projectId, uint256 _amount) external validProject(_projectId) {
+        ProjectInfo storage project = projects[_projectId];
+        require(block.number >= project.stakingStartBlockNumber, "Staking has not started yet");
+        require(block.number <= project.stakingEndBlockNumber, "Staking has ended");
+        require(_amount > 0, "Invalid stake amount");
+        require(_amount <= project.stakingLimitAmount, "Amount exceed limit stake amount");
+
         gmi.transferFrom(_msgSender(), address(this), _amount);
 
-        UserInfo storage tmpInfo = userInfo[_msgSender()][_projectId];
-        ProjectInfo storage pjInfo = projectInfo[_projectId];
+        UserInfo memory tmpUserInfo = userInfo[_projectId][_msgSender()];
 
-        if (tmpInfo.originValue == 0) {
-            tmpInfo.value = _amount;
-        } else {
-            tmpInfo.value += _amount;
+        if (tmpUserInfo.stakedAmount == 0) {
+            project.stakedAccounts.push(_msgSender());
         }
+        project.stakedTotalAmount += _amount;
+        tmpUserInfo.stakedAmount += _amount;
 
-        pjInfo.totalAmountStake += _amount;
-        tmpInfo.originValue += _amount;
-        tmpInfo.lastAction = block.timestamp; // solhint-disable-line not-rely-on-time
-       
-        emit Deposit(
-            _amount,
-            tmpInfo.lastAction
-        );
+        emit Stake(_msgSender(), _projectId, _amount);
     }
 
-    event Withdraw(uint256 indexed projectId, uint256 indexed amount, uint256 lastAction);
-
-    /// @notice withdraw amount of GMI tokens from Staking Pool
+    /// @notice claimBack amount of GMI tokens from staked GMI before
     /// @dev    This method can called by anyone
     /// @param  _projectId  id of the project
-    function withdraw(uint256 _projectId) external {
-        UserInfo storage tmpInfo = userInfo[_msgSender()][_projectId];
-        ProjectInfo storage pjInfo = projectInfo[_projectId];
-        require(pjInfo.endStakingTime < block.timestamp, "Not finished");
+    function claimBack(uint256 _projectId) external validProject(_projectId) {
+        ProjectInfo memory project = projects[_projectId];
+        require(block.number >= project.fundingEndBlockNumber, "It is not the time to claim back token");
 
-        uint256 _amount = tmpInfo.originValue;
+        UserInfo storage tmpUserInfo = userInfo[_projectId][_msgSender()];
+        uint256 claimableAmount = tmpUserInfo.stakedAmount;
+        require(claimableAmount > 0, "Nothing to claim back");
 
-        require(_amount > 0, "Nothing to withdraw");
+        tmpUserInfo.isClaimedBack = true;
+        gmi.transfer(_msgSender(), claimableAmount);
 
-        gmi.transfer(_msgSender(), _amount);
-
-        tmpInfo.value = 0;
-        tmpInfo.originValue = 0;
-        tmpInfo.lastAction = block.timestamp; // solhint-disable-line not-rely-on-time
-
-        emit Withdraw(_projectId, _amount, tmpInfo.lastAction);
+        emit ClaimBack(_msgSender(), _projectId, claimableAmount);
     }
 
-    event Funding(
-        uint256 indexed projectId,
-        uint256 indexed amount,
-        uint256 lastAction
-    );
+    function addWhitelist(uint256 _projectId, address[] memory _accounts) external onlyOwner validProject(_projectId) {
+        for (uint256 i = 0; i < _accounts.length; i++) { 
+            address account = _accounts[i];
 
-    /// @notice deposit amount of GMI tokens to Staking Pool
+            require(account != address(0), "Invalid account");
+
+            UserInfo memory tmpUserInfo = userInfo[_projectId][account];
+            require(tmpUserInfo.stakedAmount > 0, "Account did not stake");
+
+            whiteLists[_projectId][account] = true;
+        }
+        emit AddedToWhitelist(_projectId, _accounts);
+    }
+
+    function removeFromWhitelist(uint256 _projectId, address _account) public onlyOwner validProject(_projectId) {
+        whiteLists[_projectId][_account] = false;
+        emit RemovedFromWhitelist(_projectId, _account);
+    }
+
+    /// @notice fund amount of USD to funding
     /// @dev    this method can called by anyone
     /// @param  _projectId  id of the project
     /// @param  _amount  amount of the tokens to be staked
-    function funding(uint256 _projectId, uint256 _amount) external onlyWhitelisted(_projectId) {
-        require(_amount > 0, "Not enough amount");
+    function funding(uint256 _projectId, uint256 _amount) external validProject(_projectId) {
+        ProjectInfo memory project = projects[_projectId];
+        require(block.number >= project.fundingStartBlockNumber, "Funding has not started yet");
+        require(block.number <= project.fundingEndBlockNumber, "Funding has ended");
+
+        require(whiteLists[_projectId][_msgSender()], "User is not in whitelist");
+        require(_amount >= project.fundingMinAllocation, "Amount must be greater than min allocation");
+
+        uint256 fundingMaxAllocation = getFundingMaxAllocation(_projectId, _msgSender());
+        require(_amount > fundingMaxAllocation, "Amount exceed max allocation");
+
         busd.transferFrom(_msgSender(), address(this), _amount);
 
-        uint256 tokenAmount = getEstimateTokenAllocation(_amount);
+        uint256 tokenAllocationAmount = estimateTokenAllocation(_projectId, _amount);
+        UserInfo storage tmpUserInfo = userInfo[_projectId][_msgSender()];
+        tmpUserInfo.fundedAmount += _amount;
+        tmpUserInfo.tokenAllocationAmount += tokenAllocationAmount;
 
-        UserInfo storage tmpInfo = userInfo[_msgSender()][_projectId];
+        emit Funding(_msgSender(), _projectId, _amount, tokenAllocationAmount);
+    }
 
-        if (tmpInfo.originValue == 0) {
-            tmpInfo.value = _amount;
-        } else {
-            tmpInfo.value += _amount;
+    function getProjectInfo(uint256 _projectId) public view returns (ProjectInfo memory result) {
+        result = projects[_projectId];
+    }
+
+    function getUserInfo(uint256 _projectId, address _account) public view returns (UserInfo memory result) {
+        result = userInfo[_projectId][_account];
+    }
+
+    function getFundingMaxAllocation(uint256 _projectId, address _account) public view returns(uint256) {
+        UserInfo memory tmpUserInfo = userInfo[_projectId][_account];
+        uint256 stakedAmount = tmpUserInfo.stakedAmount;
+        if (stakedAmount == 0) return 0;
+
+        ProjectInfo memory project = projects[_projectId];
+        uint256 allocaltionSize = project.allocaltionSize;
+
+        uint256 maxAllocationAmount = Formula.mulDiv(stakedAmount, Formula.SCALE, project.fundingAllocationRate);
+        if (maxAllocationAmount > allocaltionSize) {
+            return allocaltionSize;
         }
 
-        tmpInfo.originValue += _amount;
-        tmpInfo.lastAction = block.timestamp; // solhint-disable-line not-rely-on-time
-        
-        emit Funding(
-            _projectId,
-            _amount,
-            tmpInfo.lastAction
-        );
+        return maxAllocationAmount;
     }
 
-    function getStakeUser(uint256 _projectId) public view returns (uint256) {
-        UserInfo storage tmpInfo = userInfo[_msgSender()][_projectId];
-        return tmpInfo.originValue;
-    }
-
-    function isWhitelisted(uint256 _projectId, address _address) public view returns(bool) { 
-        return whiteList[_address][_projectId]; 
-    }
-
-    event AddedToWhitelist(uint256 indexed projectId, address[] _accounts); 
-    function add(uint256 _projectId, address[] memory _accounts) public onlyOwner { 
-        require(_projectId > 0, "Invalid project id");
-        for (uint256 i = 0; i < _accounts.length; i++) { 
-            address account = _accounts[i]; 
-            require(account != address(0), "Invalid account provided"); 
-            whiteList[account][_projectId] = true; 
-        }
-        emit AddedToWhitelist(_projectId, _accounts); 
-    } 
-
-    event RemovedFromWhitelist(uint256 indexed projectId, address indexed account);
-    function remove(uint256 _projectId, address _address) public onlyOwner { 
-        whiteList[_address][_projectId] = false; 
-        emit RemovedFromWhitelist(_projectId, _address); 
-    }
-
-    function getEstimateTokenAllocation(uint256 _amount)
-        public
-        view
-        returns (uint256)
-    {
-        require(_amount > 0, "");
-        return SafeMath.mul(_amount, ref);
+    function estimateTokenAllocation(uint256 _projectId, uint256 _fundingAmount) public view returns (uint256) {
+        ProjectInfo memory project = projects[_projectId];
+        return Formula.mulDiv(_fundingAmount, Formula.SCALE, project.estimateTokenAllocationRate);
     }
 }
